0fb46f3237b5676bbad9fb83c98355b2af218bec
unknown <gianluca.imbiscuso@gmail.com>
2022-05-29 10:51:19 +0200

    Messing with 3D models.

--- js/classes.js
+++ js/classes.js
@@ -146,7 +146,7 @@ class locomotive {
 		this.mesh;
 		
 		// Asynchronous asset loading function. We have to wait for it to finish before we can do stuff with it.
-		const resultPromise = BABYLON.SceneLoader.ImportMeshAsync("", "https://raw.githubusercontent.com/BE3dARt/RAILBLAZER/main/assets/obj/", "Locomotive_USA.obj", scene);
+		const resultPromise = BABYLON.SceneLoader.ImportMeshAsync("", "https://raw.githubusercontent.com/BE3dARt/RAILBLAZER/main/assets/obj/", "Locomotive_USA_tex.obj", scene);
 		resultPromise.then((result) => {
 			this.mesh = result.meshes[1];
 			this.mesh.position.x = 2;
